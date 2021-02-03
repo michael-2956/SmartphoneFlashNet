6da5a1f5358f8c9b8fd2d2af69f7a72df102f38d
michael-2956 <work.michael.2956@gmail.com>
2021-02-03 15:56:54 +0200

    Edit dataset_train to accept arguments

--- dataset_train.py
+++ dataset_train.py
@@ -1,18 +1,28 @@
 """
 This module does all the steps nessesary to train the model
 """
+import sys
 from dataset_preprocessing_modules.dataset_process import filter_and_process_dataset
 from dataset_preprocessing_modules.make_vocab import extract_vocabulary
 from dataset_preprocessing_modules.dataset_to_numbers import dataset_to_numbers
 from trainNN import train_nn
 
 def main():
-    dataset_conv_filename = filter_and_process_dataset()
+    train_filename = "dataset_full.txt"
+    if len(sys.argv) == 2:
+        train_filename = sys.argv[1]
+    elif len(sys.argv) >= 3:
+        print("Usage:")
+        print(f"{sys.argv[0]} dataset.txt")
+
+    dataset_conv_filename = filter_and_process_dataset(train_filename)
     vocab_filename = extract_vocabulary(dataset_conv_filename)
     dataset_vocab_filename = dataset_to_numbers(dataset_conv_filename, vocab_filename)
     print("Dataset prepared", dataset_vocab_filename)
+
     train_nn(dataset_vocab_filename)
     print("Neural network trained", dataset_vocab_filename)
+
     print("To start generating messages, type:\n$ python3 runNN.py WORD NUMBER")
 
 
